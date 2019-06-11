d906f8323e02878befb660c1780893d83c68da58
Jinnoka3 <galinaprokofevna@gmail.com>
2019-06-11 18:02:23 +0400

    Первое задание

--- src/Project.java
+++ src/Project.java
@@ -1,16 +1,16 @@
 public class Project {
     static int x = 10;
-    static int y = 20;
-    static int z = 15;
+    static int y = 5;
+    static int z = 2;
     static int q = 7;
     public static void main(String[] args) {
 
         int lowest = getLowest(x, y);
         System.out.println(lowest);
-        int lowest1 = getLowest1(x, y, z);
+        int lowest1 = getLowest(z);
         System.out.println(lowest1);
-        int lowest1 = getLowest1(x, y, z);
-        System.out.println(lowest1);
+        int lowest2 = getLowest1(q);
+        System.out.println(lowest2);
 
     }
     public static int getLowest(int a, int b){
@@ -21,12 +21,11 @@ public class Project {
             return a;
         }
         else {
-           // System.out.println("");
             return a;
         }
     }
 
-    static int getLowest1(int c){
+    static int getLowest(int c){
         int low = getLowest(x, y);
         if(low < c){
             return low;
@@ -35,28 +34,20 @@ public class Project {
             return c;
         }
         else {
-
             return low;
         }
-
     }
 
-    static int getLowest2(int d){
-        int low = getLowest1(z);
-        if(low < d){
+    static int getLowest1(int d) {
+        int low = getLowest(z);
+        if (low < d) {
             return low;
-        }
-        else if (low > d) {
+        } else if (low > d) {
             return d;
-        }
-        else {
-
+        } else {
             return low;
         }
-
-
-
-
+    }
 }
 
 
