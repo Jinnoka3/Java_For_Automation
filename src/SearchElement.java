474c6ed88f6b1c6ce4614265a3e0123ef27f9df5
Daria Kalinina <51625026+Jinnoka3@users.noreply.github.com>
2019-06-13 13:43:43 +0000

    Practice #2 update

--- src/SearchElement.java
+++ src/SearchElement.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class SearchElement {
 
     public static void main(String[] args) {
-
         input();
     }
 
     static void input(){
         Scanner in1 = new Scanner(System.in);
-        System.out.print("Input array size");
+        System.out.print("Input array size: ");
         int num1 = in1.nextInt();
 
         Scanner in2 = new Scanner(System.in);
-        System.out.print("Input a number to search");
+        System.out.print("Input a number to search: ");
         int num2 = in2.nextInt();
         int[] array;
         array = new int[num1];
@@ -26,50 +25,51 @@ public class SearchElement {
         for (int i = 0; i < num1; i++){
             System.out.println(array[i]);
         }
-        //sort(array);
+
         System.out.println();
-        
-        long start1 = System.currentTimeMillis();
+        long t0 = System.nanoTime();
         regularSearch(array, num2);
-        long stop1 = System.currentTimeMillis();
-        long time1 = stop1 - start1;
-        System.out.println("time: " + time1);
+        long t1 = System.nanoTime();
+        System.out.println("time: " + (double)(t1 - t0));
         System.out.println();
-        long start2 = System.currentTimeMillis();
+        long t2 = System.nanoTime();
         binarySearch(array, num2);
-        long stop2 = System.currentTimeMillis();
-        long time2 = stop2 - start2;
-        System.out.println("time: " + time2);
-
-
+        long t3 = System.nanoTime();
+        System.out.println("time: " + (double)(t3 - t2));
     }
 
     static void regularSearch(int[] arr, int num){
+        System.out.print("Regular Search");
+        System.out.println();
+        boolean bool = false;
         for (int i = 0; i < arr.length; i++){
             if (arr[i] == num){
-                System.out.println(arr[i]);
-                System.out.println("true");
-                break;
+                bool = true;
+                //System.out.println(arr[i]);
             }
         }
+        System.out.println(bool);
     }
 
     static void binarySearch(int[] arr, int num){
+        boolean bool = false;
         int firstIndex = 0;
         int lastIndex = arr.length - 1;
+        System.out.print("Binary Search");
+        System.out.println();
         while (firstIndex <= lastIndex) {
             int midIndex = (firstIndex + lastIndex)/2;
             if (arr[midIndex] == num){
-                System.out.println(arr[midIndex]);
-                System.out.println("true");
+               // System.out.println(arr[midIndex]);
+                bool = true;
                 break;
             }
-            else if (arr[midIndex] < num){
+            else if (arr[midIndex] < num)
                 firstIndex = midIndex + 1;
 
-            }
             else if (arr[midIndex] > num)
                 lastIndex = midIndex - 1;
         }
+        System.out.println(bool);
     }
 }
