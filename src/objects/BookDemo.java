15cedc42b0e037dc55758cb3a195f89f5b29963a
Daria Kalinina <51625026+Jinnoka3@users.noreply.github.com>
2019-06-14 13:39:20 +0000

    Objects Basics Practice #1

--- src/objects/BookDemo.java
+++ src/objects/BookDemo.java
@@ -3,8 +3,6 @@ package objects;
 public class BookDemo {
     public static void main(String[] args) {
         Author author = new Author("Russel", "Winderand");
-        author.displayInfo();
-        System.out.println();
         Book book = new Book ("Developing Java Software", author, 79.75);
         book.displayDetails();
     }
