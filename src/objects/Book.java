d14543a5cd6f24be76daee32f533d6151c6a4572
Daria Kalinina <51625026+Jinnoka3@users.noreply.github.com>
2019-06-14 13:25:47 +0000

    Objects Basics Practice #1

--- src/objects/Book.java
+++ src/objects/Book.java
@@ -13,7 +13,7 @@ public class Book {
         //System.out.println("Book's details: " + title + " " + author.firstName + " " + author.lastName + " " + price);
         System.out.println("Book's details:" );
         System.out.println("Title " + title);
-        System.out.println("Author " + author.displayInfo());
+        System.out.println(author.displayInfo());
         System.out.println("Price " + price);
     }
 }
