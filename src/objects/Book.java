3fdcaaa0b13ac4becad166355baf68c9a2bb8a94
Daria Kalinina <51625026+Jinnoka3@users.noreply.github.com>
2019-06-14 13:40:14 +0000

    Objects Basics Practice #1

--- src/objects/Book.java
+++ src/objects/Book.java
@@ -1,9 +1,18 @@
 package objects;
 
-public class BookDemo {
-    public static void main(String[] args) {
-        Author author = new Author("Russel", "Winderand");
-        Book book = new Book ("Developing Java Software", author, 79.75);
-        book.displayDetails();
+public class Book {
+    String title;
+    Author author;
+    double price;
+    Book(String t, Author a, double p){
+        this.title = t;
+        this.author = a;
+        this.price = p;
+    }
+    protected void displayDetails(){
+        System.out.println("Book's details:" );
+        System.out.println("Title: " + title);
+        System.out.println(author.displayInfo());
+        System.out.println("Price: " + price);
     }
 }
