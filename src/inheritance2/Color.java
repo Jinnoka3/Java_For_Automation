1394113972beba2f5bd94bfd33b8411a31112b76
Daria Kalinina <51625026+Jinnoka3@users.noreply.github.com>
2019-06-21 09:04:49 +0000

    Inheritance Practice #2 update

--- src/inheritance2/Color.java
+++ src/inheritance2/Color.java
@@ -2,11 +2,26 @@ package inheritance2;
 
 public enum Color {
 
-    BLACK,
-    WHITE,
-    YELLOW,
-    RED,
-    BLUE,
-    GREEN
+    BLACK ("black"),
+    WHITE ("white"),
+    YELLOW ("yellow"),
+    RED ("red"),
+    BLUE ("blue"),
+    GREEN ("green");
+
+    private String color;
+
+    Color(String color){
+        this.color = color;
+    }
+
+    public static boolean checkColor (String str){
+        for (Color c : Color.values()) {
+            if (c.name().equals(str.toUpperCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
