05f537ae95885493b55b2534b50ae2cb10a7d0b9
Daria Kalinina <51625026+Jinnoka3@users.noreply.github.com>
2019-06-21 09:05:22 +0000

    Inheritance Practice #2 update

--- src/inheritance2/Demo.java
+++ src/inheritance2/Demo.java
@@ -4,105 +4,39 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
-        double radius;
-        double width;
-        double length;
-        double side;
-
-        Scanner in = new Scanner(System.in);
-        //Shape
-        System.out.println("Enter shape's color:");
-        String str = in.nextLine();
-
-        if (checkColor(str) == false) {
-            System.out.println("Wrong color");
-        } else {
-            System.out.println("Enter filled (true/false):");
-            boolean filled = in.nextBoolean();
-            Shape shape = new Shape(str, filled);
-            System.out.println(shape);
-        }
 
         //Circle
-        Scanner in1 = new Scanner(System.in);
-        System.out.println("Enter circle's color:");
-        String str1 = in1.nextLine();
-
-        if (checkColor(str1) == false) {
-            System.out.println("Wrong color");
-        } else {
-            System.out.println("Enter filled (true/false):");
-            boolean filled1 = in1.nextBoolean();
-            System.out.println("Enter circle's radius:");
-            radius = in1.nextDouble();
-
-            if (radius <= 0) {
-                System.out.println("Wrong radius");
-                System.exit(0);
-            }
-
-            Circle circle = new Circle(radius, str1, filled1);
-            System.out.println(circle);
-            System.out.println("Area: " + circle.getArea());
-        }
-
+        System.out.println("Circle");
+            Circle circle = new Circle();
+            circle.input();
+        System.out.println(circle);
         //Rectangle
-        Scanner in2 = new Scanner(System.in);
-        System.out.println("Enter rectangle's color:");
-        String str2 = in2.nextLine();
-
-        if (checkColor(str2) == false) {
-            System.out.println("Wrong color");
-        } else {
-            System.out.println("Enter filled (true/false):");
-            boolean filled2 = in2.nextBoolean();
-            System.out.println("Enter width:");
-            width = in2.nextDouble();
-            if (width <= 0) {
-                System.out.println("Wrong width");
-                System.exit(0);
-            }
-            System.out.println("Enter length:");
-            length = in2.nextDouble();
-            if (length <= 0) {
-                System.out.println("Wrong length");
-                System.exit(0);
-            }
-
-            Rectangle rectangle = new Rectangle(width, length, str2, filled2);
-            System.out.println(rectangle);
-            System.out.println("Perimeter: " + rectangle.getPerimeter());
-        }
-
+        System.out.println("Rectangle");
+            Rectangle rectangle = new Rectangle();
+            rectangle.input();
+        System.out.println(rectangle);
         //Square
-        Scanner in3 = new Scanner(System.in);
-        System.out.println("Enter square's color:");
-        String str3 = in3.nextLine();
+        System.out.println("Square");
+            Square square = new Square();
+            square.input();
+        System.out.println(square);
 
-        if (checkColor(str3) == false) {
-            System.out.println("Wrong color");
-        } else {
-            System.out.println("Enter filled (true/false):");
-            boolean filled3 = in3.nextBoolean();
-            System.out.println("Enter side:");
-            side = in3.nextDouble();
-            if (side <= 0) {
-                System.out.println("Wrong width");
-                System.exit(0);
+        Shape[] array = new Shape[]{circle, rectangle, square};
+        for (Shape shape : array){
+            if (shape instanceof Circle){
+                System.out.println("Circle's area: " + ((Circle)shape).getArea());
+                continue;
             }
-
-            Square square = new Square(side, str3, filled3);
-            System.out.println(square);
-            System.out.println("Perimeter: " + square.getPerimeter());
-        }
-
-    }
-    public static boolean checkColor (String str){
-        for (Color c : Color.values()) {
-            if (c.name().equals(str.toUpperCase())) {
-                return true;
+            if (shape instanceof Square){
+                System.out.println("Square's perimeter: " + ((Square)shape).getPerimeter());
+                continue;
+            }
+            if (shape instanceof Rectangle){
+                System.out.println("Rectangle's perimeter: " + ((Rectangle)shape).getPerimeter());
+                continue;
             }
         }
-        return false;
     }
+
 }
+
